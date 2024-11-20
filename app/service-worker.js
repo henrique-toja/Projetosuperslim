3658bb2269edf4120330022412c647a18d3939e5
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2024-11-20 10:25:29 -0300

    Atualizar o service-worker.js

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,42 +1,22 @@
-const CACHE_NAME = "super-slim-cache-v1";
-const urlsToCache = [
-  "/",
-  "/app/",
-  "/app/app.css",
-  "/assets/icon.ico",
-  "/assets/icon-192x192.png",
-  "/assets/icon-512x512.png"
-];
-
-// Instalação do Service Worker
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado.');
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open('super-slim-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/app/index.html',
+        '/app/app.css',
+        '/assets/icon-192x192.png',
+        '/assets/icon-512x512.png',
+      ]);
     })
   );
 });
 
-// Ativação do Service Worker
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Interceptação de requisições
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
