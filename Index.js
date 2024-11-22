e939df508af88d8c4c0fbcdf88e79ef61001ddaf
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2024-11-22 08:11:59 -0300

    Atualizar o

---
+++ Index.js
@@ -0,0 +1,26 @@
+{
+  "name": "projetosuperslim-app",
+  "version": "1.0.0",
+  "description": "Aplicativo web do Projeto Super Slim",
+  "main": "index.js",
+  "scripts": {
+    "start": "npm run dev",
+    "build": "react-scripts build",
+    "dev": "react-scripts start",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "dependencies": {
+    "axios": "^1.0.0",  // Para requisições HTTP
+    "react": "^18.0.0", // Framework de frontend
+    "react-dom": "^18.0.0", // Integração com DOM
+    "react-router-dom": "^6.0.0", // Para navegação no React
+    "react-scripts": "^5.0.0" // Ferramentas para React
+  },
+  "devDependencies": {
+    "webpack": "^5.0.0", // Pacote de empacotamento
+    "webpack-cli": "^5.0.0" // Ferramentas de CLI do Webpack
+  },
+  "author": "Projeto Super Slim",
+  "license": "MIT"
+}
